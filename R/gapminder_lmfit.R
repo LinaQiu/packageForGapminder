c657b8acc17f5ce1e7e5ca50151f67c161ed3291
Lina Qiu <qln19940518@gmail.com>
2015-11-20 22:50:19 -0800

    Update DESCRIPTION, LICENSE.

--- R/gapminder_lmfit.R
+++ R/gapminder_lmfit.R
@@ -1,3 +1,21 @@
+#' @title Model Population VS Year for countries in GAPMINDER dataset.
+#'
+#' @description gapminder_lmfit is used to fit linear models to population and time for countries in GAPMINDER dataset.
+#' It can be used to carry out regression between population and time, and return the coefficients, residuals
+#' and fitError(=sum of residuals^2) back at the same time.
+#'
+#' @param data a data frame, containing the variables "population" and "year" for a country in GAPMINDER dataset.
+#' @param offset this can be used to specify an a priori known component to be included in the linear predictor
+#' during fitting. In this function, the default value of offset is 1952, because we want to make the intersect
+#' of fitted linear model be the predicted value for year 1952, the year when GAPMINDER dataset began to record
+#' data.
+#'
+#' @return list
+#' @export
+#' @examples
+#' canadaData <- gapminder %>% filter(country=="Canada") %>% arrange(year) %>% select(year,pop)
+#' gapminder_lmfit(canadaData)
+#'
 gapminder_lmfit <- function(data,offset=1952)
 {
   results <- vector("list",4)
